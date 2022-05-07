7e811c7530a6f0cbac803ca08c9493591331e840
Ashley Coles <ashley@acoles.co.uk>
2022-05-07 20:20:17 +0100

    Removing use statement

--- src/Validator.php
+++ src/Validator.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace ValidationMiddleware;
 
-use http\Env\Request;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;

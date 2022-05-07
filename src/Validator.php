a5f9d27db6883a066b87ce15dcffdd77551e7e49
Ashley Coles <ashley@acoles.co.uk>
2022-05-07 16:33:01 +0100

    removing dep and fixing typo

--- src/Validator.php
+++ src/Validator.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace ValidationMiddlewear;
+namespace ValidationMiddleware;
 
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
